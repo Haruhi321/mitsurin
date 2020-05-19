7f2f8234d767ba64aab05cbc64de2a9a44e6340b
Haruhi321 <ropf123@mail.ru>
2020-05-19 14:18:45 +0400

    no message

---
+++ clientserverchat/IChatService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace clientserverchat
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IChatService" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface IChatService
+    {
+        [OperationContract]
+        int Connect();
+
+        [OperationContract]
+        void Disconnect(int id);
+
+        [OperationContract]
+        void SendMessage(string message);
+    }
+}
